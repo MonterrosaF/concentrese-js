31679f415152dc4d3dc2c10d9f77678118164fbd
Felipe Monterrosa <felipe.monterrosa@hotmail.com>
2020-07-31 13:55:30 -0500

    CREATION CLASS MEMORAMA

---
+++ js/script.js
@@ -0,0 +1,13 @@
+class Memorama {
+  constructor() {
+    this.totalCards = [];
+    this.Cardsnumber = 0;
+    this.cardVerifier = [];
+    this.errors = 0;
+    this.difficultyLevel = '';
+    this.correctsImages = [];
+    this.pusherCards = [];
+  }
+}
+
+new Memorama();
