17b84d598f12dac1e20db5c158218383f86e8302
Felipe Monterrosa <felipe.monterrosa@hotmail.com>
2020-07-31 14:11:20 -0500

    CAPTURING DATA JSON AND SORT CARDS

--- js/script.js
+++ js/script.js
@@ -4,9 +4,36 @@ class Memorama {
     this.Cardsnumber = 0;
     this.cardVerifier = [];
     this.errors = 0;
-    this.difficultyLevel = '';
+    this.difficultyLevel = "";
     this.correctsImages = [];
     this.pusherCards = [];
+
+    this.$generalContainer = document.querySelector(".general-container");
+    this.$cardsContainer = document.querySelector(".cards-container");
+    this.$lockedScreen = document.querySelector(".locked-screen");
+    this.$message = document.querySelector(".message");
+
+    this.eventsListener();
+  }
+
+  eventsListener() {
+    window.addEventListener("DOMContentLoaded", () => {
+      this.screenLoader();
+    });
+  }
+
+  async screenLoader() {
+    const response = await fetch("../memo.json");
+    const data = await response.json();
+    this.totalCards = data;
+
+    if (this.totalCards.length > 0) {
+      this.totalCards.sort(order);
+      function order(a, b) {
+        return Math.random() - 0.5;
+      }
+    }
+    console.log(this.totalCards)
   }
 }
 
